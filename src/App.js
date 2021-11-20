cb0e0b3e30473ca8cdf382a3e6e5371a1e1fa630
zubair <muhammad.zubair.i.am@gmail.com>
2021-11-20 11:37:13 +0500

    adjusted starting app text position

--- src/App.js
+++ src/App.js
@@ -50,7 +50,12 @@ function App() {
     findIdByLatLong(searchLocation);
   }, [searchLocation, findIdByLatLong]);
 
-  if (loading) return <>Loading data</>;
+  if (loading)
+    return (
+      <div className="flex justify-center items-center text-lg w-screen h-screen">
+        <div>Loading Weather Data..........</div>
+      </div>
+    );
 
   return (
     <div className="text-textColor flex flex-col md:flex-row min-h-screen max-w-full font-raleway text-base">
