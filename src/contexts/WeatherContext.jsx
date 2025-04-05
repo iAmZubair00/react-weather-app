2776ea5e02be8fa1359ea74c3da8f49cb0cbf964
ZubairDevDev <m.zubair@adalfi.com>
2025-04-05 21:20:10 +0500

    set default location to current

--- src/contexts/WeatherContext.jsx
+++ src/contexts/WeatherContext.jsx
@@ -1,11 +1,11 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { getRequiredWeather } from "../utils/utils";
 
 const WeatherContext = createContext({});
 
 const WeatherProvider = ({ children }) => {
-  const [searchLocation, setSearchLocation] = useState("lahore");
+  const [searchLocation, setSearchLocation] = useState(null);
   const [searchNeeded, setSearchNeeded] = useState(false);
   const [tempScale, setTempScale] = useState("c");
 
@@ -29,6 +29,18 @@ const WeatherProvider = ({ children }) => {
     tempScale,
     setTempScale,
   };
+
+  // set default location to current location
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setSearchLocation(() => {
+        return {
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        };
+      });
+    });
+  }, []);
 
   return (
     <WeatherContext.Provider value={contextValue}>
